9d3368208e92476fd6a040c4ed99f05a17af46cb
Joshua Rea <joshdanielrea@gmail.com>
2021-03-08 16:23:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  let array = [];
-  for(let i = 0; i < musicians.length; i++){
-    array.push(`${musicians{i}} plays ${instruments{i}}`)
-  }
-  return array;
-}
